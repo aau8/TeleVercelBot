dde5088873f290d41e8ba5405cddf467136cefcc
Artem Ugryumov <artiom.ygryumov@gmail.com>
2023-03-24 19:58:04 +0500

    change receiveUpdates call

--- api/telegram.mjs
+++ api/telegram.mjs
@@ -1,4 +1,12 @@
 import {start} from "telebot-vercel"
 import bot from "../bot.mjs"
 
-export default start({bot})
+// export default start({bot})
+
+export default async ({ body }, { json }) => {
+	let response = {status: false};
+	response = await bot?.receiveUpdates([body]);
+	// console.log('ok')
+	console.log(response)
+	// json(response)
+}
